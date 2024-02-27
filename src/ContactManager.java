a9b947c2d2aaa0aebd444503adbf5c8a28498cc6
admin <johanna-london@hotmail.fr>
2024-02-27 16:24:13 +0100

    ajout des méthodes ajouter et affficher contact

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,7 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ContactManager {
-    List<Contact> gestionnaireContact= new ArrayList <>();
 
+public class ContactManager {
+    public static List<Contact> gestionnaireContact= new ArrayList <>();
+    public static void ajouterContact(Contact contact) {
+        gestionnaireContact.add(contact);
+    }
+    public void afficherContacts() {
+        for (Contact contact : gestionnaireContact) {
+            System.out.println(contact.toString());
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Contact johanna = new Contact("Guichard", "Johanna", "0654321987", "johanna.guichard@hotmail.fr");
-
         Contact nerina = new Contact("Vaudelin", "Nérina", "0672109479", "n.vaudelin@orange.fr");
+        Contact contact1 = new Contact("Doe", "John", "0987654321", "johndoe@gmail.fr");
 
 
     }
