1c8dd97d8595c23bb36a7b6725ae917eb45ce8e4
unknown <n.vaudelin@orange.fr>
2024-02-27 15:42:25 +0100

    ajout méthodes modifications contactManager

--- src/ContactManager.java
+++ src/ContactManager.java
@@ -1,7 +1,45 @@
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ContactManager {
-    List<Contact> gestionnaireContact= new ArrayList <>();
+    public static List<Contact> gestionnaireContact= new ArrayList <>();
+
+    public static void supprimerContact (Contact contact){
+
+        gestionnaireContact.remove(contact);
+
+    }
+
+    public static void modifierContact (int indexContact){
+
+        Scanner scan = new Scanner(System.in);
+        String reponse = "";
+        String nvllInfoContact = "";
+        System.out.println("Quelle innformation souhaitez-vous modifier ? (nom, prenom, numeroTelephone, adresseMail");
+        reponse = scan.nextLine();
+        System.out.println(("Quelle est la nouvelle valeur de la donnée ? "));
+        nvllInfoContact = scan.nextLine();
+
+        switch (reponse) {
+            case "nom":
+                gestionnaireContact.get(indexContact).setNom(nvllInfoContact);
+                break;
+            case "prenom":
+                gestionnaireContact.get(indexContact).setPrenom(nvllInfoContact);
+                break;
+            case "numeroTelephone":
+                gestionnaireContact.get(indexContact).setNumeroTelephone(nvllInfoContact);
+                break;
+            case "adresseMail":
+                gestionnaireContact.get(indexContact).setAdresseMail(nvllInfoContact);
+                break;
+            default:
+                System.out.println("La réponse est invalide.");
+        }
+
+        System.out.println(gestionnaireContact);
+    }
 
 }
