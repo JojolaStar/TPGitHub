4cbec06875c7568d4686542bf97dad850dc0597a
unknown <johanna-london@hotmail.fr>
2024-02-27 11:52:59 +0100

    ajout de contact dans main avec différentes branches

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Contact johanna = new Contact("Guichard", "Johanna", "0654321987", "johanna.guichard@hotmail.fr");
 
     }
 }
