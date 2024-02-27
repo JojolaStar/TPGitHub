4a388a83045fbafbe5f79e6f92035d3ad1e69c1a
unknown <n.vaudelin@orange.fr>
2024-02-27 15:50:47 +0100

    merge priseRDV

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         Contact johanna = new Contact("Guichard", "Johanna", "0654321987", "johanna.guichard@hotmail.fr");
-
         Contact nerina = new Contact("Vaudelin", "Nérina", "0672109479", "n.vaudelin@orange.fr");
+        Contact test = new Contact("Tintin", "Milou", "54664688", "njhi@lie.fr");
 
+
+        ContactManager.gestionnaireContact.add(johanna);
+        ContactManager.gestionnaireContact.add(nerina);
+        ContactManager.gestionnaireContact.add(test);
+        System.out.println(ContactManager.gestionnaireContact);
+
+        ContactManager.supprimerContact(test);
+
+        System.out.println(ContactManager.gestionnaireContact);
+
+        ContactManager.modifierContact(0);
 
     }
 }
